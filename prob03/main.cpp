0aa16e7bea50a73d9147e22fc563207bf8ea3ef4
jmurkijanian <jmurkijanian@gmail.com>
2019-09-14 14:26:46 -0700

    wrote program that calculates ComapanyX's sales for 2019

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,21 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+//Given values, and Constants
+const long TOTALSALES = 2103419277;
+const double PERCENTINCREASE = 0.18;
+long predictedsales2;
+
+//Calculates predicted sales
+long predictedsales = TOTALSALES * PERCENTINCREASE;
+predictedsales2 = predictedsales + TOTALSALES;
+
+//Presents Info
+std::cout << "Last year's sales were " <<TOTALSALES<< " dollars\n";
+std::cout << "This year's sales prediction " <<predictedsales2<< " dollars\n";
+
+return 0;
+}
